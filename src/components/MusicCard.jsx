f17f80b57c76727db6acaa02694f7bf8cbe586db
Leandro Batista de Carvalho <leandrobcrv@gmail.com>
2022-11-09 03:20:32 -0300

    Requisito 9

--- src/components/MusicCard.jsx
+++ src/components/MusicCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { addSong } from '../services/favoriteSongsAPI';
+import { addSong, getFavoriteSongs } from '../services/favoriteSongsAPI';
 import Loading from './Loading';
 
 class MusicCard extends React.Component {
@@ -9,17 +9,27 @@ class MusicCard extends React.Component {
     checked: false,
   };
 
-  handleChange = async () => {
+  async componentDidMount() {
     const { musicaObj } = this.props;
+    const musicasFav = await getFavoriteSongs(musicaObj);
+    if (musicasFav.some((e) => e.trackId === musicaObj.trackId)) {
+      this.setState({ checked: true });
+    } else {
+      this.setState({ checked: false });
+    }
+  }
+
+  handleChange = async () => {
     const { checked } = this.state;
-    this.setState({ carregando: true });
-    await addSong(musicaObj);
-    this.setState({ carregando: false });
+    const { musicaObj } = this.props;
 
     if (checked === true) {
       this.setState({ checked: false });
     } else {
       this.setState({ checked: true });
+      this.setState({ carregando: true });
+      await addSong(musicaObj);
+      this.setState({ carregando: false });
     }
   };
 
